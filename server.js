f5ae674dbcce56e600d330014aec4a09cb956893
HannaTrinh <trinhmaianhacg@gmail.com>
2024-10-10 00:23:34 -0400

    Remove default rendering in server

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const path = require('path');
 const app = express();
 
 mongoose
-  .connect('mongodb+srv://ronit:N92cBmF6HKaUb39J@cluster0.x828y.mongodb.net/myAppDb')
+  .connect('mongodb+srv://ronit:N92cBmF6HKaUb39J@cluster0.x828y.mongodb.net/myAppDb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log('MongoDB Connected'))
   .catch((err) => console.log(err));
 
@@ -16,16 +19,18 @@ app.use(session({
   secret: 'secretkey',
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: process.env.NODE_ENV === 'production' }
+  cookie: { maxAge: 1000 * 60 * 60 * 24 } // 1 day
 }));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.get('/', (req, res) => {
-  res.redirect('/auth/login');
-});
+
+// app.get('/', (req, res) => {
+//   res.redirect('/auth/login');
+// });
 const authRoutes = require('./routes/auth');
 const eventRoutes = require('./routes/events');
+
 app.use('/auth', authRoutes);
 app.use('/events', eventRoutes);
 
@@ -36,3 +41,5 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+module.exports = app;
