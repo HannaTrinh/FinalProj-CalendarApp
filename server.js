05978c6eb4ed587751433aa1767d14a62b8f4730
HannaTrinh <trinhmaianhacg@gmail.com>
2024-10-09 23:38:34 -0400

    Fixed for deployment

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
-const bcrypt = require('bcryptjs');
 const path = require('path');
 const app = express();
+
 mongoose
   .connect('mongodb+srv://ronit:N92cBmF6HKaUb39J@cluster0.x828y.mongodb.net/myAppDb')
   .then(() => console.log('MongoDB Connected'))
   .catch((err) => console.log(err));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(session({
   secret: 'secretkey',
   resave: false,
-  saveUninitialized: true
+  saveUninitialized: true,
+  cookie: { secure: process.env.NODE_ENV === 'production' }
 }));
-app.use(express.urlencoded({ extended: false }));
+
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
 app.get('/', (req, res) => {
   res.redirect('/auth/login');
 });
